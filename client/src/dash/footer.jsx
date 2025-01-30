dbe6bb80f3e65a5ce250c56d7b48062abcffbb3e
Maiza Aymen <aymenmaiza87@gmail.com>
2025-01-30 09:25:48 +0100

    `Update French translations in Footer component`

--- client/src/dash/footer.jsx
+++ client/src/dash/footer.jsx
@@ -4,22 +4,22 @@ function Footer() {
         <div class="max-w-screen-xl mx-auto">
           <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             <div class="sm:col-span-2 max-w-sm">
-              <h4 class="text-base font-semibold mb-6 text-white">About Us</h4>
+              <h4 class="text-base font-semibold mb-6 text-white">À propos de nous</h4>
               <p class="text-gray-400 text-sm">HostCycle centralise les informations de vos clients et assure un suivi proactif des échéances d'hébergement pour une gestion sans souci.</p>
             </div>
   
             <div>
               <h4 class="text-base font-semibold mb-6 text-white">Services</h4>
               <ul class="space-y-3">
-                <li><a href="javascript:void(0)" class="text-gray-400 hover:text-white text-sm">Real-time collaboration and preview features</a></li>
-                <li><a href="javascript:void(0)" class="text-gray-400 hover:text-white text-sm">build and deploy web projects with ease</a>
+                <li><a href="javascript:void(0)" class="text-gray-400 hover:text-white text-sm">Fonctionnalités de collaboration et d'aperçu en temps réel</a></li>
+                <li><a href="javascript:void(0)" class="text-gray-400 hover:text-white text-sm">Créez et déployez des projets Web en toute simplicité</a>
                 </li>
-                <li><a href="javascript:void(0)" class="text-gray-400 hover:text-white text-sm">Easy integration with your backend</a></li>
+                <li><a href="javascript:void(0)" class="text-gray-400 hover:text-white text-sm">Intégration facile avec votre backend</a></li>
               </ul>
             </div>
   
             <div class="space-y-3">
-              <h4 class="text-base font-semibold mb-6 text-white">Contact Us</h4>
+              <h4 class="text-base font-semibold mb-6 text-white">Contactez-nous</h4>
               <p class="text-gray-400 text-sm">123 Main Street</p>
               <p class="text-gray-400 text-sm">City, State, Country</p>
               <p class="text-gray-400 text-sm">ITQANLABS@GMAIL.COM</p>
