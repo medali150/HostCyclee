bfb74dcd47628d00bd4e77521d752a6096003389
Maiza Aymen <aymenmaiza87@gmail.com>
2025-02-02 21:14:07 +0100

    "Update Hero component phrase with minor formatting change"

--- client/src/dash/hero.jsx
+++ client/src/dash/hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
 
   const phrases = [
     `
-How are you ${userData?.name.toUpperCase()|| 'visiteur'} ?`
+How are you ${userData?.name.toUpperCase() || 'visiteur'} ?`
   ];
 
   useEffect(() => {
