1ef07b735ff0cd4a1e3ff3dd79d6506f2ab153bf
Maiza Aymen <aymenmaiza87@gmail.com>
2025-02-21 12:23:57 +0100

    `Refactor logout function in authController.js`

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -169,21 +169,21 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout=async(req,res)=>{
-    try{
-        res.clearCookie('token',{
-                httpOnly:true,
-                secure:process.env.NODE_ENV==='production',
-                sameSite:process.env.NODE_ENV==='production' 
-              
-            })
-            return res.json({success:true,message:"logged out"});
+export const logout = async (req, res) => {
+  try {
+      res.clearCookie('token', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          expires: new Date(0), // Force l'expiration du cookie immédiatement
+      });
 
-    }catch(error){
-        return res.json({success: false, message: error.message}) //lmessage ili 3y5arjah error
+      return res.status(200).json({ success: true, message: "Logged out successfully" });
+  } catch (error) {
+      return res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-    }
-}
 export const sendVerifyOtp= async(req,res)=>{
     try {
         const {userId}=req.body;
