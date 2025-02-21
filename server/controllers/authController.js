904d230d52040e10e0cd351bc7abb3145c16ce6d
Maiza Aymen <aymenmaiza87@gmail.com>
2025-02-21 12:13:13 +0100

    `Removed sameSite option from res.clearCookie call`

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -174,8 +174,7 @@ export const logout=async(req,res)=>{
         res.clearCookie('token',{
                 httpOnly:true,
                 secure:process.env.NODE_ENV==='production',
-                sameSite:process.env.NODE_ENV==='production' ?
-                'none':'strict',
+                sameSite:process.env.NODE_ENV==='production' 
               
             })
             return res.json({success:true,message:"logged out"});
